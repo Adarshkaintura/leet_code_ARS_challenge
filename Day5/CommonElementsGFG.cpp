59a1a5c0f8bd4edca500c97db295eafc337b84ad
Adarsh Kaintura <146357978+Adarshkaintura@users.noreply.github.com>
2024-02-25 15:50:06 +0530

    Update CommonElementsGFG.cpp

--- Day5/CommonElementsGFG.cpp
+++ Day5/CommonElementsGFG.cpp
@@ -37,3 +37,28 @@ vector <int> commonElements (int A[], int B[], int C[], int n1, int n2, int n3)
            vector<int> res(ans.begin(),ans.end());
             return res;
         }
+//best and optimum approach................
+vector <int> commonElements (int a[], int b[], int c[], int n1, int n2, int n3)
+        {
+           int i=0;
+           int j=0;
+           int k=0;
+           set<int> ans;
+           while(i<n1&& j<n2&& k<n3){
+               if(a[i]==b[j]&&b[j]==c[k]){
+                   ans.insert(a[i]);
+                   i++;
+                   j++;
+                   k++;
+               }else if(a[i]<b[j]){
+                   i++;
+               }else if(b[j]<c[k]){
+                   j++;
+               }else{
+                   k++;
+               }
+              
+           }
+           vector<int> res(ans.begin(),ans.end());
+           return res;
+        }
