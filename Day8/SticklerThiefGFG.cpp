028392f8c13f6ff8e840ad6dcb2fbdeb76d43f7d
Adarsh Kaintura <146357978+Adarshkaintura@users.noreply.github.com>
2024-02-27 15:54:29 +0530

    Update SticklerThiefGFG.cpp

--- Day8/SticklerThiefGFG.cpp
+++ Day8/SticklerThiefGFG.cpp
@@ -19,3 +19,24 @@ int FindMaxSum(int arr[], int n)
       }
       return b[n-1];
     }
+
+//optimized approach is this with space coplexity O(1) but i still can't figure out how this is created what is the working or maths behind it
+int FindMaxSum(vector<int> arr, int n)
+{
+    int incl = arr[0];
+    int excl = 0;
+    int excl_new;
+    int i;
+ 
+    for (i = 1; i < n; i++) {
+        // Current max excluding i
+        excl_new = max(incl, excl);
+ 
+        // Current max including i
+        incl = excl + arr[i];
+        excl = excl_new;
+    }
+ 
+    // Return max of incl and excl
+    return max(incl, excl);
+}
