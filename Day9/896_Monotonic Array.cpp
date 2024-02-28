932a6ae886bcda597e7cb5d2a2cf8a25b9b8ad57
Adarsh Kaintura <146357978+Adarshkaintura@users.noreply.github.com>
2024-02-28 22:03:12 +0530

    Update 896_Monotonic Array.cpp

--- Day9/896_Monotonic Array.cpp
+++ Day9/896_Monotonic Array.cpp
@@ -20,3 +20,19 @@ public:
     }
 };
 //optimized
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+      int res=0;
+      for(int i=0;i<nums.size()-1;i++){
+          if(nums[i]<nums[i+1]){
+              if(res==0) res=1;
+              else if(res==-1)return false;
+          }else if(nums[i]>nums[i+1]){
+              if(res==0)res=-1;
+              else if(res==1) return false;
+          }
+      }
+      return true;
+    }
+};
