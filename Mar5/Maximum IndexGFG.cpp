8453be99f33e5d4bd86d2231202df2290c97b81a
Adarsh Kaintura <146357978+Adarshkaintura@users.noreply.github.com>
2024-03-05 14:29:35 +0530

    Update Maximum IndexGFG.cpp

--- Mar5/Maximum IndexGFG.cpp
+++ Mar5/Maximum IndexGFG.cpp
@@ -19,3 +19,34 @@ brute force */
     }
 
 // optimized
+ int maxIndexDiff(int arr[], int n) 
+    { 
+        if (n <= 1)
+        return 0; // or any meaningful value
+
+     vector<int> low(n,INT_MAX);
+     vector<int> high(n,INT_MIN);
+     int l=0;
+     int h=n-1;
+     low[l]=arr[l];
+     int ans=0;
+     high[h]=arr[h];
+     for(int i=1;i<n;i++){
+         low[i]=min(low[i-1],arr[i]);
+     }
+      for(int i=n-2;i>=0;--i){
+         high[i]=max(high[i+1],arr[i]);
+     }
+     l=0;
+     int r=0;
+     while(l<n && r<n){
+         if(low[l]<=high[r]){
+              ans=max(ans,r-l);
+              r++;
+         }else{
+             l++;
+         }
+     }
+     return ans;
+    }
+};
