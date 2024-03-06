f3440d3f017bcd13bd1b70e73eff158d20d1fb70
Adarsh Kaintura <146357978+Adarshkaintura@users.noreply.github.com>
2024-03-06 18:53:00 +0530

    Update LongestSubstringWithoutRepeatingCharacters .cpp

--- 01_25questionChallenge/Moderate/LongestSubstringWithoutRepeatingCharacters .cpp
+++ 01_25questionChallenge/Moderate/LongestSubstringWithoutRepeatingCharacters .cpp
@@ -1,3 +1,25 @@
+//sliding window approach
+int uniqueSubstrings(string input) {
+    unordered_map<char, int> freq;
+    int start = 0;
+    int maxLen = 0;
+
+    for (int end = 0; end < input.size(); end++) {
+        char currentChar = input[end];
+        freq[currentChar]++;
+
+        while (freq[currentChar] > 1) {
+            char leftChar = input[start];
+            freq[leftChar]--;
+            start++;
+        }
+
+        maxLen = max(maxLen, end - start + 1);
+    }
+
+    return maxLen - 1; 
+}
+//the below code didn't work i dont know why its not working even though the logic is correct 
 #include <bits/stdc++.h> 
 int uniqueSubstrings(string input)
 {
@@ -18,5 +40,27 @@ int uniqueSubstrings(string input)
         }
     }
     ans = max(temp, ans);
+    return ans;
+}
+//below is also a good code but also failed in some test cases 
+#include <bits/stdc++.h> 
+int uniqueSubstrings(string input)
+{
+    int ans=0;
+    int temp=0;
+    unordered_map<char,int> mp;
+    for(int i=0;i<input.size();i++){
+        mp[input[i]]++;
+        if(mp[input[i]]==1){
+            temp++;
+        }else{
+            ans=max(ans,temp);
+            temp=1;
+            mp.clear();
+            mp[input[i]]++;
+
+        }
+    }
+    ans=max(ans,temp);
     return ans;
 }
