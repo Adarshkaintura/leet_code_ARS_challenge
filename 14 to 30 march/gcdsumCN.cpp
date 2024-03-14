5cab44f8fd376e7cd8dbb47ef0af2f43c28c9d34
Adarsh Kaintura <146357978+Adarshkaintura@users.noreply.github.com>
2024-03-14 19:35:31 +0530

    Create gcdsumCN.cpp

---
+++ 14 to 30 march/gcdsumCN.cpp
@@ -0,0 +1,23 @@
+// Consider all numbers from 1 to ‘N’, your task is to find the sum of gcd of all pairs (i, j) such that 1 <= i < j <= N.
+
+// For example for N = 3, all pairs are { (1, 2), (1, 3), (2, 3) }.
+
+// Note :
+
+// Gcd of two numbers (X, Y) is defined as the largest integer that divides both ‘X’ and ‘Y’
+long long gcd(int a,int b){
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+long long gcdSum(int n)
+{
+
+    long long count=0;
+    for(int i=1;i<n;i++){
+        for(int j=i+1;j<=n;j++){
+            count+=gcd(i,j);
+        }
+    }
+    return count;
+}
