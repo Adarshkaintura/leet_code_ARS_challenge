9c059c6e1b42906469d8a6c2b5a2ffdbc682a6d5
Adarsh Kaintura <146357978+Adarshkaintura@users.noreply.github.com>
2024-03-30 20:51:13 +0530

    Create 992. Subarrays with K Different Integers.cpp

---
+++ 14 to 30 march/992. Subarrays with K Different Integers.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+ int slidingwindow(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        int ans = 0;
+        int i = 0, j = 0;
+        while (j < nums.size()) {
+            mp[nums[j]]++;
+            while (mp.size() > k) {
+                mp[nums[i]]--;
+                if (mp[nums[i]] == 0)
+                    mp.erase(nums[i]);
+                i++;
+            }
+            ans += j - i + 1; 
+            j++;
+        }
+        return ans;
+    }
+
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return slidingwindow(nums, k) - slidingwindow(nums, k - 1);
+    }
+};
